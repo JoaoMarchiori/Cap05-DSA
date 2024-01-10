56bb91bb185e0de5d721a6a2942e87b11ada2cc4
João Marchiori <joaomarchiori99@gmail.com>
2024-01-10 20:11:37 -0300

    Create ex02.py

---
+++ 41-Cap05/Notebooks/ex02.py
@@ -0,0 +1,10 @@
+# Exercício 2 - Crie uam função que receba uma string como argumento e retorne a mesma string em letras maiúsculas.
+# Faça uma chamada à função, passando como parâmetro uma string
+
+def minn():
+    minus = fra.upper()    
+    print(minus)
+
+
+fra = str(input("Escreva um texto: "))
+minn()
